33eb1dcefd61147811858aa35007ade301c0287c
Roc Boronat <roc@fewlaps.com>
2016-01-12 18:57:55 +0100

    Loading a picture from the Internet and placing it in the huge ImageView

--- app/src/main/java/com/schibsted/introducingandroid/MainActivity.java
+++ app/src/main/java/com/schibsted/introducingandroid/MainActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,6 +31,14 @@ public class MainActivity extends AppCompatActivity {
                     image.setVisibility(View.VISIBLE);
                     image.startAnimation(AnimationUtils.loadAnimation(MainActivity.this, android.R.anim.fade_in));
                 }
+            }
+        });
+
+        findViewById(R.id.bt_load_picture_from_internet).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ImageView backgroundIV = (ImageView) findViewById(R.id.iv_background);
+                Picasso.with(MainActivity.this).load("http://i.imgur.com/DvpvklR.png").into(backgroundIV);
             }
         });
     }
