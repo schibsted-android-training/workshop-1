6f4bb8f3cc3b46f4fe77f304b6b0abe01ffd70ac
Roc Boronat <roc@fewlaps.com>
2016-01-12 17:58:30 +0100

    Giving an id to the Button and referencing it from the Activity

--- app/src/main/java/com/schibsted/introducingandroid/MainActivity.java
+++ app/src/main/java/com/schibsted/introducingandroid/MainActivity.java
@@ -2,6 +2,7 @@ package com.schibsted.introducingandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.bt_example);
     }
 }
