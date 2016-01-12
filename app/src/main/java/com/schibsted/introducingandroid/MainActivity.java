c15e8c6dbb4c79ece7485c24e2d0d4392aad204b
Roc Boronat <roc@fewlaps.com>
2016-01-12 18:29:43 +0100

    Referencing the ImageView from the Activity and making it invisible when the user taps the Button

--- app/src/main/java/com/schibsted/introducingandroid/MainActivity.java
+++ app/src/main/java/com/schibsted/introducingandroid/MainActivity.java
@@ -12,12 +12,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final View image = findViewById(R.id.iv_example);
         final Button button = (Button) findViewById(R.id.bt_example);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String text = getString(R.string.you_tapped_me);
                 button.setText(text);
+
+                image.setVisibility(View.INVISIBLE);
             }
         });
     }
