9a1a79f918a890df99af29f23554dc3eff327f58
Roc Boronat <roc@fewlaps.com>
2016-01-12 18:32:57 +0100

    Animating the ImageView visibility change with a fade_in and a fade_out animation

--- app/src/main/java/com/schibsted/introducingandroid/MainActivity.java
+++ app/src/main/java/com/schibsted/introducingandroid/MainActivity.java
@@ -3,6 +3,7 @@ package com.schibsted.introducingandroid;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,8 +23,10 @@ public class MainActivity extends AppCompatActivity {
 
                 if (image.getVisibility() == View.VISIBLE) {
                     image.setVisibility(View.INVISIBLE);
+                    image.startAnimation(AnimationUtils.loadAnimation(MainActivity.this, android.R.anim.fade_out));
                 } else {
                     image.setVisibility(View.VISIBLE);
+                    image.startAnimation(AnimationUtils.loadAnimation(MainActivity.this, android.R.anim.fade_in));
                 }
             }
         });
