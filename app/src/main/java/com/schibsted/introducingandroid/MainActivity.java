82e161a90ac8f6d68e61db9880e3a28898ef16d1
Roc Boronat <roc@fewlaps.com>
2016-01-12 18:02:32 +0100

    Extracting the hardcoded text of the Activity to the strings.xml, and referencing it via the R class

--- app/src/main/java/com/schibsted/introducingandroid/MainActivity.java
+++ app/src/main/java/com/schibsted/introducingandroid/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                button.setText("You tapped me!");
+                button.setText(R.string.you_tapped_me);
             }
         });
     }
